c68734c4e2a6e4f2fd1361decb0d378808e51898
FinnTew <nanomoa76@gmail.com>
2025-01-07 18:52:30 +0800

    update

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+@app.route('/')
+def index():
+    return 'FlashSale'
+
+@app.get('/ping')
+def ping():
+    print("success")
+    return 'pong'
+
 def main():
-    print("FlashSale")
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
 if __name__ == '__main__':
     main()
